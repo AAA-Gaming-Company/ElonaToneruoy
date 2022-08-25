f0f8e956502e2749e90621b3ccc6856ab489df65
Charles <charlesglucas@gmail.com>
2022-08-25 10:45:16 +0200

    Added feedback for pickup

--- Assets/Scripts/Player/FryingPan.cs
+++ Assets/Scripts/Player/FryingPan.cs
@@ -1,17 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MoreMountains.Feedbacks;
 
 public class FryingPan : MonoBehaviour
 {
     public LayerMask playerLayer;
+    public MMF_Player pickupFeedback;
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.GetComponent<PlayerController>())
         {
             collision.GetComponent<PlayerController>().hasFryingPan = true;
-            Destroy(gameObject);
+            pickupFeedback.PlayFeedbacks();
         }
     }
 }
